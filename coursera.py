6f95fcb56dc22210ec80070bea8e9fbf044b48ce
Андрей Аракельянц <arakelyants.andrey@yandex.ru>
2018-05-10 14:38:38 +0300

    Make the first working version.

--- coursera.py
+++ coursera.py
@@ -1,21 +1,26 @@
-# 1. с сайта Курсеры взять 20 курсов; +
-# 2. со страницы каждого курса взять его х-ки: название, язык, дата начала,
-# кол-во недель, средняя оценка;
-# 3. Загрузить данные по каждому курсу в файл xlsx.
 import requests
 from bs4 import BeautifulSoup
 import random
+from openpyxl import Workbook
+import argparse
 
 
 def get_console_arguments():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-o',
+        '--output_file_path',
+        help='Enter the full path of output file or its name only.'
+    )
+    arguments = parser.parse_args()
+    return arguments
 
 
 def download_course_info(courses_info_url):
     print('send request...')
     coursera_response = requests.get(courses_info_url)
     course_info = coursera_response.text
-    print('get response')
+    print('got response')
     return course_info
 
 
@@ -32,7 +37,7 @@ def get_links_of_random_courses(xml_info_string, coursers_number):
     return links_of_random_courses
 
 
-def get_course_info(page):
+def get_course_info_from_html(page):
     bs_object = BeautifulSoup(page, 'html.parser')
     course_title = bs_object.find(
         'h1',
@@ -40,45 +45,88 @@ def get_course_info(page):
     ).get_text()
     language = bs_object.find('div', class_='rc-Language').get_text()
     start_date = bs_object.find('div', id='start-date-string').get_text()
-    try:
-        week_items = bs_object.find('div', class_='rc-WeekView').contents
-        weeks_number = len(week_items)
-    except AttributeError:
+    week_items = bs_object.find('div', class_='rc-WeekView')
+    if week_items:
+        weeks_number = len(week_items.contents)
+    if not week_items:
         weeks_number = None
-    course_mean_mark = bs_object.find(
-        'div',
-        class_='ratings-text bt3-hidden-xs'
-    ).span.get_text()
-    return course_title, language, start_date, weeks_number, course_mean_mark
+    course_mark_html_item = bs_object.find(
+            'div',
+            class_='ratings-text headline-2-text'
+        )
+    if course_mark_html_item:
+        course_mean_mark = course_mark_html_item.span.get_text()
+    if not course_mark_html_item:
+        course_mean_mark = None
+    course_info_dict = {
+        'course_title': course_title,
+        'course_language': language,
+        'course_start_date': start_date,
+        'course_duration': weeks_number,
+        'course_mean_mark': course_mean_mark
+    }
+    return course_info_dict
 
 
-def get_all_courses_info(courses_links):
+def get_all_courses_list(courses_links):
     course_list = []
     for course_link in courses_links:
         course_page = download_course_info(course_link)
-        course_info = get_course_info(course_page)
+        course_info = get_course_info_from_html(course_page)
         course_list.append(course_info)
     return course_list
 
 
+def set_column_names(table):
+    column_names = [
+        'Course Name', 'Language', 'Start Date', 'Weeks Number', 'Course Mean Mark'
+    ]
+    table.append(column_names)
+
+
+def set_course_info(table, course_dict):
+    course_title = course_dict['course_title']
+    course_language = course_dict['course_language']
+    course_start_date = course_dict['course_start_date']
+    course_duration = course_dict['course_duration']
+    course_mean_mark = course_dict['course_mean_mark']
+    course_features = (
+        course_title,
+        course_language,
+        course_start_date,
+        course_duration,
+        course_mean_mark,
+    )
+    table.append(course_features)
+
+
+def get_output_path(file_path):
+    if not file_path:
+        return 'courses_info.xlsx'
+    return file_path
+
+
 def output_courses_info_to_xlsx(path, courses_info):
-    pass
+    workbook = Workbook()
+    worksheet = workbook.active
+    set_column_names(worksheet)
+    for course in courses_info:
+        set_course_info(worksheet, course)
+    output_file_path = path
+    if not output_file_path:
+        output_file_path = 'courses_info.xlsx'
+    workbook.save(output_file_path)
 
 
 if __name__ == '__main__':
-    # args = get_console_arguments()
-    # file_path = args.file_path
+    number_of_coursers = 20
     coursera_info_url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    # courses_string_info = download_course_info(coursera_info_url)
-    with open('courses.xml', 'r') as xml_file:
-        courses_string_info = xml_file.read()
-    number_of_coursers = 2
+    console_arguments = get_console_arguments()
+    file_path = console_arguments.output_file_path
+    courses_xml_info = download_course_info(coursera_info_url)
     list_of_course_links = get_links_of_random_courses(
-        courses_string_info,
+        courses_xml_info,
         number_of_coursers
     )
-    print(list_of_course_links)
-    all_courses_info = get_all_courses_info(list_of_course_links)
-    print(all_courses_info)
-    file_path = None
+    all_courses_info = get_all_courses_list(list_of_course_links)
     output_courses_info_to_xlsx(file_path, all_courses_info)
