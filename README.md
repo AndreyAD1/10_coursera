2926b6879e3b59ff1893197febf0947311a403fa
Андрей Аракельянц <arakelyants.andrey@yandex.ru>
2018-05-10 15:28:20 +0300

    Output course URL, make readme and requirements.

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Coursera Dump
 
-[TODO. There will be project description]
+This script saves in *.xlsx file information about 20 random
+courses of [Coursera](https://www.coursera.org). 
+
+# How To Install
+
+Python v3.5 should be already installed. 
+Afterwards use pip (or pip3 if there is a conflict with old Python 2 setup)
+to install dependecies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+Remember that it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) 
+for better isolation.
+
+# Quick Start
+
+The script has one optional argument - full path of output file.
+By default the script outputs result in `courses_info.xlsx` located
+in script`s directory.
+
+Output file contains these course features: name,
+language, start date, weeks of study, course rating and URL.
+
+To run script on Linux:
+```bash
+$ python coursera.py -o courses.xlsx
+```
+Windows usage is the same.
 
 # Project Goals
 
--- coursera.py
+++ coursera.py
@@ -17,19 +17,17 @@ def get_console_arguments():
 
 
 def download_course_info(courses_info_url):
-    print('send request...')
     coursera_response = requests.get(courses_info_url)
     course_info = coursera_response.text
-    print('got response')
     return course_info
 
 
-def get_links_of_random_courses(xml_info_string, coursers_number):
+def get_links_of_random_courses(xml_info_string, courses_number):
     bs_object = BeautifulSoup(xml_info_string, 'lxml')
     course_links_with_tags = bs_object.find_all('loc')
     random_links_with_tags = random.sample(
         course_links_with_tags,
-        coursers_number
+        courses_number
     )
     links_of_random_courses = [
         link_with_tags.get_text() for link_with_tags in random_links_with_tags
@@ -37,7 +35,7 @@ def get_links_of_random_courses(xml_info_string, coursers_number):
     return links_of_random_courses
 
 
-def get_course_info_from_html(page):
+def get_course_info_from_html(page, url):
     bs_object = BeautifulSoup(page, 'html.parser')
     course_title = bs_object.find(
         'h1',
@@ -63,7 +61,8 @@ def get_course_info_from_html(page):
         'course_language': language,
         'course_start_date': start_date,
         'course_duration': weeks_number,
-        'course_mean_mark': course_mean_mark
+        'course_mean_mark': course_mean_mark,
+        'course_url': url
     }
     return course_info_dict
 
@@ -72,46 +71,43 @@ def get_all_courses_list(courses_links):
     course_list = []
     for course_link in courses_links:
         course_page = download_course_info(course_link)
-        course_info = get_course_info_from_html(course_page)
+        course_info = get_course_info_from_html(course_page, course_link)
         course_list.append(course_info)
     return course_list
 
 
-def set_column_names(table):
+def write_column_names(table):
     column_names = [
-        'Course Name', 'Language', 'Start Date', 'Weeks Number', 'Course Mean Mark'
+        'Course Name', 'Language', 'Start Date', 'Weeks of Study',
+        'Course Mean Mark', 'Course URL'
     ]
     table.append(column_names)
 
 
-def set_course_info(table, course_dict):
+def write_course_info(table, course_dict):
     course_title = course_dict['course_title']
     course_language = course_dict['course_language']
     course_start_date = course_dict['course_start_date']
     course_duration = course_dict['course_duration']
     course_mean_mark = course_dict['course_mean_mark']
+    course_url = course_dict['course_url']
     course_features = (
         course_title,
         course_language,
         course_start_date,
         course_duration,
         course_mean_mark,
+        course_url
     )
     table.append(course_features)
-
-
-def get_output_path(file_path):
-    if not file_path:
-        return 'courses_info.xlsx'
-    return file_path
 
 
 def output_courses_info_to_xlsx(path, courses_info):
     workbook = Workbook()
     worksheet = workbook.active
-    set_column_names(worksheet)
+    write_column_names(worksheet)
     for course in courses_info:
-        set_course_info(worksheet, course)
+        write_course_info(worksheet, course)
     output_file_path = path
     if not output_file_path:
         output_file_path = 'courses_info.xlsx'
@@ -119,14 +115,14 @@ def output_courses_info_to_xlsx(path, courses_info):
 
 
 if __name__ == '__main__':
-    number_of_coursers = 20
+    number_of_courses = 20
     coursera_info_url = 'https://www.coursera.org/sitemap~www~courses.xml'
     console_arguments = get_console_arguments()
-    file_path = console_arguments.output_file_path
+    output_path = console_arguments.output_file_path
     courses_xml_info = download_course_info(coursera_info_url)
     list_of_course_links = get_links_of_random_courses(
         courses_xml_info,
-        number_of_coursers
+        number_of_courses
     )
     all_courses_info = get_all_courses_list(list_of_course_links)
-    output_courses_info_to_xlsx(file_path, all_courses_info)
+    output_courses_info_to_xlsx(output_path, all_courses_info)
